027ba243d9e8583cc5c5a8c2ee180b15b584ac1b
BrunoBafilli <144603933+lognsoft@users.noreply.github.com>
2024-02-27 13:42:37 -0300

    Outra maneira de usar dois ponteiros
    Outra maneira de usar dois ponteiros

---
+++ Módulo 3 - Array, vetor e string/Aula 15 - Outra maneira de usar dois ponteiros.cs
@@ -0,0 +1,56 @@
+string s1 = "adefiejijffadefiejijff";
+string s2 = "abcdefg";
+
+void ON()
+{
+	int i = 0;
+	int t = 0;
+	int c = 0;
+	int passos = 0;
+	
+	while (i < s1.Length && t < s2.Length)
+	{
+		passos++;
+		if (s1[i] == s2[t])
+		{
+			i++;
+			c++;
+		}
+		t++;
+	}
+	Console.WriteLine(c == s1.Length);
+	Console.WriteLine("ON "+passos);
+}
+
+
+ON();
+Console.WriteLine("----------------");
+ON2();
+
+void ON2()
+{
+	int passos = 0;
+	int c = 0;
+	int ultima = 0;
+
+//O(n)
+	for (int i = 0; i < s1.Length; i++) //O(n)
+	{
+		for (int t = ultima; t < s2.Length; t++) //O(n)
+		{
+			passos++;
+			if (s1[i] == s2[t])
+			{
+				if (s1[i] == s2[t])
+				{
+					ultima = t + 1;
+					c++;
+					break;
+				}
+			}
+		}
+	}
+
+	Console.WriteLine(c == s1.Length);
+	Console.WriteLine("ON2 "+passos);
+}
