b45b8b12e4ece57d5db10705f652436db651fc3f
BrunoBafilli <144603933+lognsoft@users.noreply.github.com>
2024-03-13 12:52:33 -0300

    Janela deslizante, caso de uso
    Janela deslizante, caso de uso

---
+++ Módulo 3 - Array, vetor e string/Aula 21 - Janela deslizante, caso de uso.cs
@@ -0,0 +1,36 @@
+﻿string binario = "111001100101";
+
+int resultado = maiorSequencia();
+
+Console.WriteLine(resultado);
+
+int maiorSequencia()
+{
+	int esq = 0;
+	int res = 0;
+	int z = 0;
+
+	for (int dir = 0; dir < binario.Length; dir++)
+	{
+		if (binario[dir] == '0')
+		{
+			z++;
+		}
+
+		while (z > 1)
+		{
+			if (binario[esq] == '0')
+			{
+				z--;
+			}
+			esq++;
+		}
+
+		int x = dir - esq + 1;
+		
+		res = res < x ? x : res;
+
+	}
+	
+	return res;
+}
