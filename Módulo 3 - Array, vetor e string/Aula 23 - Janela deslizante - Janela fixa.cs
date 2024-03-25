67178427c9b162ef5385a8231491c51628c3551e
BrunoBafilli <144603933+lognsoft@users.noreply.github.com>
2024-03-25 15:56:42 -0300

    janela fixa
    janela fixa

---
+++ Módulo 3 - Array, vetor e string/Aula 23 - Janela deslizante - Janela fixa.cs
@@ -0,0 +1,27 @@
+﻿int[] nums = new []{3,2,5,6,6};
+
+int t = 2;
+int somaAtual = 0;
+int maior = 0;
+
+for (int i = 0; i < t; i++)
+{
+	somaAtual += nums[i];
+}
+
+int resposta = somaAtual;
+
+for (int i = t; i < nums.Length; i++)
+{
+	somaAtual += nums[i] - nums[i - t];
+
+	if (somaAtual > resposta)
+	{
+		resposta = somaAtual;
+	}
+	//3,2,5,6,6
+}
+
+Console.WriteLine(resposta);
+
+
