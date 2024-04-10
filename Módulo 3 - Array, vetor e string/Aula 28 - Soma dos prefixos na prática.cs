a90057af2b2b4c1e8747bc06ca4f91741614e1b6
BrunoBafilli <brunobafilli@gmail.com>
2024-04-10 09:58:31 -0300

    Aula 28 - Soma dos prefixos na prática
    Aula 28 - Soma dos prefixos na prática

---
+++ Módulo 3 - Array, vetor e string/Aula 28 - Soma dos prefixos na prática.cs
@@ -0,0 +1,25 @@
+﻿int[] nums = new int[] { 3, 2, 4, 5, 5 };
+int[] prefixo = new int[nums.Length];
+
+prefixo[0] = nums[0];
+
+for (int i = 1; i < prefixo.Length; i++)
+{
+    prefixo[i] = nums[i] + prefixo[i - 1];
+}
+
+int inicio = 0;
+int fim = 4;
+
+int resultado;
+
+if (inicio == 0)
+{
+    resultado = prefixo[fim];
+}
+else
+{
+    resultado = prefixo[fim] - prefixo[inicio - 1];
+}
+
+Console.WriteLine(resultado);
